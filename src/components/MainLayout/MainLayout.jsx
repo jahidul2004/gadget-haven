3f171e4314de0dc24f68ce7fe30d80282b1ac6d0
jahidul2004 <islamjahiduljihad@gmail.com>
2024-11-05 16:01:38 +0600

    Initial project setup Compleate.

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,21 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            {/* Header start */}
+
+            {/* Header end */}
+
+            {/* Gadget start */}
+
+            {/* Gadget start */}
+
+            {/* Gadget end */}
+
+            {/* Footer start */}
+            {/* Footer end */}
+        </div>
+    );
+};
+
+export default MainLayout;
