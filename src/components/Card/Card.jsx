5d0e4928a3562d55b9593516f5fe42b1f219e349
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-13 19:07:13 +0600

    Card design updated.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,15 +6,21 @@ const Card = ({ data }) => {
     return (
         <div className="bg-[#f7f7f7] p-4 rounded-lg">
             <div className="w-full h-[200px] bg-[#dadce2] rounded-lg">
-                <img className="w-full h-full rounded-lg object-cover" src={image} alt={title} />
+                <img
+                    className="w-full h-full rounded-lg object-cover"
+                    src={image}
+                    alt={title}
+                />
             </div>
             <div>
                 <h1 className="text-xl font-bold mt-3">{title}</h1>
-                <p className="font-semibold mb-4 text-[#0000007c]">Price: ${price}</p>
+                <p className="font-semibold mb-4 text-[#0000007c]">
+                    Price: ${price}
+                </p>
             </div>
             <Link
                 to={`/product/${product_id}`}
-                className="btn border-[#9538e1] rounded-3xl text-[#9538e1]"
+                className="btn bg-[#9538e1] rounded-lg text-white"
             >
                 View Details
             </Link>
